881339fbf6a8b2b0e1b969fbb4c6985afa45a4a3
Sumit Pugalia <sumit_pugalia@yahoo.com>
2026-02-01 00:37:11 +0530

    version 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Replayx.MixProject do
   def project do
     [
       app: :replayx,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
